c0b4e1fb7931ce86afcb9c84916cd445df668f4f
Marshal-AM <marshal.25ec@licet.ac.in>
2025-07-04 13:37:23 +0530

    Added Disaster Struct and Mapping

--- Flow contract/GodsHand.sol
+++ Flow contract/GodsHand.sol
@@ -4,6 +4,14 @@ pragma solidity ^0.8.19;
 contract GodsHand {
     address public owner;
 
+    struct Disaster {
+        string title;
+        uint256 targetAmount;
+        address creator;
+    }
+
+    mapping(bytes32 => Disaster) public disasters;
+
     constructor() {
         owner = msg.sender;
     }
