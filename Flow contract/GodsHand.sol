a86949b80a353a59e92cc072e3d121f790fdd894
Marshal-AM <marshal.25ec@licet.ac.in>
2025-07-04 13:37:57 +0530

    Create disaster function added

--- Flow contract/GodsHand.sol
+++ Flow contract/GodsHand.sol
@@ -12,7 +12,18 @@ contract GodsHand {
 
     mapping(bytes32 => Disaster) public disasters;
 
+    event DisasterCreated(bytes32 indexed disasterHash, string title, address indexed creator, uint256 targetAmount);
+
     constructor() {
         owner = msg.sender;
+    }
+
+    function createDisaster(string memory _title, uint256 _targetAmount) public returns (bytes32) {
+        require(bytes(_title).length > 0, "Title required");
+        require(_targetAmount > 0, "Target must be > 0");
+        bytes32 disasterHash = keccak256(abi.encodePacked(_title, msg.sender, block.timestamp));
+        disasters[disasterHash] = Disaster(_title, _targetAmount, msg.sender);
+        emit DisasterCreated(disasterHash, _title, msg.sender, _targetAmount);
+        return disasterHash;
     }
 }
